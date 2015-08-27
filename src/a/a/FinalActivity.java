deefe10b15947b6fb93a968e2b4cd4e36edead78
Ahmad Ezzat <androidandroid2011@yahoo.com>
2015-08-27 07:59:50 +0200

    someone final
    someone desc

---
+++ src/a/a/FinalActivity.java
@@ -0,0 +1,13 @@
+package a.a;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FinalActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
